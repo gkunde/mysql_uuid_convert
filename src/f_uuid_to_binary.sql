5f0f0e2a5ff2fdb58f5a340d5a4bd32b0ddd4c08
Garrett Kunde <34901900+gkunde@users.noreply.github.com>
2020-11-22 06:32:49 -0600

    Create f_uuid_to_binary.sql

---
+++ src/f_uuid_to_binary.sql
@@ -0,0 +1,8 @@
+CREATE FUNCTION `f_uuid_to_bin`(_uuid BINARY(36)) RETURNS BINARY(16)
+RETURN
+        UNHEX(CONCAT(
+            SUBSTR(_uuid, 15, 4),
+            SUBSTR(_uuid, 10, 4),
+            SUBSTR(_uuid,  1, 8),
+            SUBSTR(_uuid, 20, 4),
+            SUBSTR(_uuid, 25) ))
