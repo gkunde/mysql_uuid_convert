7d9e2b4d53cf1ac214495c9b99cbb779019b2971
Garrett Kunde <34901900+gkunde@users.noreply.github.com>
2020-11-22 06:33:45 -0600

    Create f_uuid_from_binary.sql

---
+++ src/f_uuid_from_binary.sql
@@ -0,0 +1,9 @@
+CREATE FUNCTION `f_uuid_from_bin`(_bin BINARY(16)) RETURNS VARCHAR(36) CHARSET utf8mb4
+RETURN
+        LCASE(CONCAT_WS('-',
+            HEX(SUBSTR(_bin,  5, 4)),
+            HEX(SUBSTR(_bin,  3, 2)),
+            HEX(SUBSTR(_bin,  1, 2)),
+            HEX(SUBSTR(_bin,  9, 2)),
+            HEX(SUBSTR(_bin, 11))
+                 ))
