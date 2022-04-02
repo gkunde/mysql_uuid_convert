1cb606d58c601ae7187a1b90ec2e5ba4d498f36e
Garrett Kunde <34901900+gkunde@users.noreply.github.com>
2022-04-02 10:55:48 -0500

    Make compatible for Binary Logging
    Update for configurations of MySQL, where functions require Deterministic characteristic for Binary Logging

--- src/convert_uuid_to_binary.sql
+++ src/convert_uuid_to_binary.sql
@@ -1,6 +1,9 @@
-CREATE FUNCTION `convert_uuid_to_bin`(val BINARY(36)) RETURNS BINARY(16)
-RETURN
-    UNHEX(
+CREATE FUNCTION `convert_uuid_to_bin`(
+	val BINARY(36)
+) RETURNS binary(16)
+    DETERMINISTIC
+RETURN 
+	UNHEX(
         CONCAT(
             SUBSTR(val, 15, 4),
             SUBSTR(val, 10, 4),
